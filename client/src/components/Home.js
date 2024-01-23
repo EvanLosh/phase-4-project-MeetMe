21c48d0fca5597dabcabb475bcf2080c15271339
sean innella <sean.innella@gmail.com>
2024-01-23 02:07:33 -0500

    fetch user appointments

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,3 +9,25 @@ function Home() {
 }
 
 export default Home;
+
+// import React, { useEffect, useState } from "react";
+// import RenderCalendar from "./RenderCalendar";
+
+// function Home() {
+//     const [appointments, setAppointments] = useState([]);
+
+//     useEffect(() => {
+//         fetch('api url goes here')
+//             .then(response => response.json())
+//             .then(data => setAppointments(data))
+//             .catch(error => console.error('Error:', error));
+//     }, []);
+
+//     return (
+//         <div id="home">
+//             <RenderCalendar appointments={appointments} />
+//         </div>
+//     );
+// }
+
+// export default Home;
