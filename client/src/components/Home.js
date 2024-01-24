a0030b9b5d6a0510e8bec4114df0a654620a6315
sean innella <sean.innella@gmail.com>
2024-01-24 02:18:17 -0500

    retrieve appointments from the api

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,12 +3,22 @@ import RenderCalendar from "./RenderCalendar";
 import AppointmentsForm from "./AppointmentsForm";
 import "./Home.css"
 
-
 function Home() {
-    return <div id="home">
-        <RenderCalendar />
-        <AppointmentsForm />
-    </div>;
+    const [appointments, setAppointments] = useState([]);
+
+    useEffect(() => {
+        fetch('/appointments/user_id')  // Replace 'user_id' with the actual user ID
+            .then(response => response.json())
+            .then(data => setAppointments(data))
+            .catch(error => console.error('Error:', error));
+    }, []);  // The empty array means this effect runs once when the component mounts
+
+    return (
+        <div id="home">
+            <RenderCalendar appointments={appointments} />
+            <AppointmentsForm />
+        </div>
+    );
 }
 
 export default Home;
