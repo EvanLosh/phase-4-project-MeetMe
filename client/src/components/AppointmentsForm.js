d6046fca3f920e9d1bbe0ec982bd3561309643eb
Shal Malhotra <smalhotra0109@gmail.com>
2024-01-25 12:05:19 -0500

    fixed some issues

--- client/src/components/AppointmentsForm.js
+++ client/src/components/AppointmentsForm.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import CreateAppointment from "./CreateAppointment";
 import ViewAppointment from "./ViewAppointment";
 import ModifyAppointment from "./ModifyAppointment";
-import "./AppointmentsForm.css"
-import { useParams } from "react-router-dom"
+import "./AppointmentsForm.css";
+import { useParams } from "react-router-dom";
 
 const blankAppointment = {
     title: '',
@@ -19,88 +19,74 @@ const blankAppointment = {
             status: ''
         }
     ]
-}
+};
 
 function stringifyAttendeesJSON(list) {
-    let stringify = ''
+    let stringify = '';
     for (let i = 0; i < list.length; i++) {
         if (i === (list.length - 1)) {
-            stringify = stringify + list[i].username
-        }
-        else {
-            stringify = stringify + list[i].username + ", "
+            stringify = stringify + list[i].username;
+        } else {
+            stringify = stringify + list[i].username + ", ";
         }
     }
-    return stringify
+    return stringify;
 }
 
 function jsonifyAttendeesString(string) {
-    let attendees = []
-    let stringList = string.split(", ")
+    let attendees = [];
+    let stringList = string.split(", ");
     for (let i = 0; i < stringList.length; i++) {
-        attendees.push({ username: stringList[i], status: 'Uncomfirmed' })
+        attendees.push({ username: stringList[i], status: 'Unconfirmed' });
     }
-    return attendees
+    return attendees;
 }
 
-
-
-function AppointmentsForm({ appointments, theUser, serverURL, addAppointment }) {
-
-    const { child, id } = useParams()
-    // const [appointment, setAppointment] = useState(blankAppointment)
+function AppointmentsForm() {
+    const { child, id } = useParams();
+    const [appointment, setAppointment] = useState(blankAppointment);
     // { fetch the appointment by id from /appointments/<int:id>  }
 
-    // const attendees = appointment.attendees.map((attendee) => {
-    //     return <p key={attendee.username}>{attendee.username}: {attendee.status}</p>
-    // })
+    const attendees = appointment.attendees.map((attendee) => {
+        return <p key={attendee.username}>{attendee.username}: {attendee.status}</p>;
+    });
 
-    // const submitAppointment = async (method) => {
-    //     const requestOptions = {
-    //         method: method.toUpperCase(),
-    //         headers: {
-    //             'Accept': 'application/json',
-    //             // Add any other headers needed?
-    //         },
-    //         body: formData,
-    //     };
-    // }
+    const submitAppointment = async (method) => {
+        const requestOptions = {
+            method: method.toUpperCase(),
+            headers: {
+                'Accept': 'application/json',
+                // Add any other headers needed?
+            },
+            body: JSON.stringify(appointment), // Use appointment instead of formData
+        };
+        // Fetch logic here using requestOptions
+    };
 
     function chooseForm(child, id = -1) {
-
-        let appointment = appointments.filter((a) => { return a.id === id })[0]
-        if (!appointment) {
-            appointment = blankAppointment
-        }
-        const userIsOwner = (theUser.id === appointment.owner_id)
-
-
-        // One of the following components gets rendered
-
         if (child === "view") {
-            return <ViewAppointment id={id} appointment={appointment} stringifyAttendeesJSON={stringifyAttendeesJSON} />
+            return <ViewAppointment id={id} appointment={appointment} attendees={attendees} />;
+        } else if (child === "modify") {
+            return <ModifyAppointment id={id} appointment={appointment} attendees={attendees} jsonifyAttendeesString={jsonifyAttendeesString} />;
+        } else {
+            return <CreateAppointment jsonifyAttendeesString={jsonifyAttendeesString} />;
         }
-        else if (child === "modify") {
-
-            return <ModifyAppointment appointment={appointment} jsonifyAttendeesString={jsonifyAttendeesString} userIsOwner={userIsOwner} />
-
-        }
-        else {
-            return <CreateAppointment jsonifyAttendeesString={jsonifyAttendeesString} addAppointment={addAppointment} />
-        }
-
     }
 
-    return <div id="appointments-form">
-        <div id="appointment-form-options">
-            <a href="/">Create</a>
-            <a href={"/view/" + id}>View</a>
-            <a href={"/modify/" + id}>Modify</a>
-
+    return (
+        <div id="appointments-form">
+            <p>appointments form</p>
+            <div id="appointment-form-options">
+                <a href="/">Create</a>
+                <a href={"/view/" + id}>View</a>
+                <a href={"/modify/" + id}>Modify</a>
+            </div>
+            {chooseForm(child, id)}
+            <button onClick={() => submitAppointment('post')}>Create Appointment</button>
+            <button onClick={() => submitAppointment('patch')}>Update Appointment</button>
+            <button onClick={() => submitAppointment('delete')}>Delete Appointment</button>
         </div>
-        {chooseForm(child, id)}
-    </div>
-
+    );
 }
 
 export default AppointmentsForm;
